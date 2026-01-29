ef375d89fc08392194f3599d292dd54e13228649
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-01-29 22:32:28 +0530

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -6,7 +6,7 @@ cout<<value_to_print<<value_to_print;
 Any number of values can be printed using one command as shown.
 The printf command comes from C language. It accepts an optional format specification and a list of variables. Two examples for printing a string are:
 printf("%s", string); printf(string);
-Note that neither method adds a newline. It only prints what you tell it to. them
+Note that neither method adds a newline. It only prints what you tell it to do.
 
 Sample Output
 Hello, World!
